9661ccde77c49e848357874e11346a40084b4cc9
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-28 20:23:57 -0500

    start routing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const Express = require('express');
 const app = express()
 const PORT = process.env.PORT || 3000
 
+//Parsing HTML req
+const got = require('got');
+
+
 //// Public viewery ////
 app.use(express.static('client'))
 
@@ -18,16 +22,25 @@ const { get } = require('request')
 const giphyGenerator = require('./jeffGif');
 
 
-//// Init Functionality for Requested Image Replace
-get(url, (err, _, body) => {
-    $ = load(body)
-    $('img').each(function(i, img){
-      $(img).attr('src', giphyGenerator())
+app.get('/:url', (req, res) => {
+  const url = req.params.url;
+  //// Init Functionality for Requested Image Replace
+  got(url)
+    .then((response)=> {
+      console.log(response.body)
+      $ = load(response.body)
+      $('img').each(function(i, img){
+        $(img).attr('src', giphyGenerator())
+      })
+      $ = $.html()
+      res.send($)
     })
-    $ = $.html()
-    res.end($)
+    .catch((error) => {
+      console.log(error.response.body)
+    })
   }
 })
 
-//// You know, like, listen on the port or something something darkside 
+
+//// You know, like, listen on the port or something something darkside
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
