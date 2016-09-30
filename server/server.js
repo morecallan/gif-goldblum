9b2f11061b8fe81ec7283c0fe7790675542351d2
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-29 22:06:12 -0500

    user input parsing mostly done.

--- server/server.js
+++ server/server.js
@@ -11,13 +11,63 @@ const bodyParser = require('body-parser');
 // const got = require('got');
 const request = require('request');
 
+var validUrl = require('valid-url');
+
+
+const quickCheck = (suspect) => {
+  return validUrl.isUri(suspect) ? true : false;
+}
+
+const checkRequest = (inputUrl) => {
+
+  let prefixedInputUrl = `www.${inputUrl}`
+
+  console.log(prefixedInputUrl)
+  return new Promise((resolve, reject) => {
+      request.get(`${prefixedInputUrl}`, (err, _, body) => {
+          if (!err) {
+            resolve(`${prefixedInputUrl}`)
+          } else {
+            request.get(`http://${prefixedInputUrl}`, (err, _, body) => {
+              if (!err) {
+                resolve(`http://${prefixedInputUrl}`)
+              } else {
+                request.get(`http://${inputUrl}`, (err, _, body) => {
+                  if (!err) {
+                    resolve(`http://${inputUrl}`)
+                  } else {
+                    request.get(`https://${prefixedInputUrl}`, (err, _, body) => {
+                      if (!err) {
+                        resolve(`https://${prefixedInputUrl}`)
+                      } else {
+                        request.get(`https://${inputUrl}`, (err, _, body) => {
+                          if (!err) {
+                            resolve(`https://${inputUrl}`)
+                          } else {
+                            reject(prefixedInputUrl)
+                          }
+                        })
+                      }
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
+      })
+  }
+
+
+
+
+
 //// Public viewery ////
 app.use(express.static('client'))
 
 
 //// Interaction with requested website ////
 const { load } = require('cheerio')
-//const { get } = require('request')
 
 //// Random Jeff Gif from scraper ////
 const {giphyGenerator} = require('./jeffGif');
@@ -27,17 +77,22 @@ const {giphyGenerator} = require('./jeffGif');
 
 
 app.get('/gif/:url', (req, res) => {
-  const url = req.params.url;
-  console.log(url)
-  //// Init Functionality for Requested Image Replace
-  request.get(`https://${url}`, (err, _, body) => {
-    let $ = load(body)
-    $('img').each(function(i, img){
-      $(img).attr('src', giphyGenerator())
+  let url = req.params.url;
+  checkRequest(url)
+    .then((correctUrl) => {
+    let usableUrl = correctUrl
+    console.log(usableUrl)
+    //// Init Functionality for Requested Image Replace
+    request.get(usableUrl, (err, _, body) => {
+      let $ = load(body)
+      $('img').each(function(i, img){
+        $(img).attr('src', giphyGenerator())
+      })
+      $ = $.html()
+      res.send($)
     })
-    $ = $.html()
-    res.send($)
   })
+  .catch(console.error)
 })
 
 
